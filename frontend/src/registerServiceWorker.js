76e2a40f4919208bde13ce4409e00793f779b31a
Trevor <trevor.slaton@gmail.com>
2017-09-17 14:59:18 -0700

    style: Remove more semicolons

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -16,7 +16,7 @@ const isLocalhost = Boolean(
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
-);
+)
 
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
@@ -26,7 +26,7 @@ export default function register() {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
-      return;
+      return
     }
 
     window.addEventListener('load', () => {
@@ -39,7 +39,7 @@ export default function register() {
         // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl)
       }
-    });
+    })
   }
 }
 
@@ -64,12 +64,12 @@ function registerValidSW(swUrl) {
               console.log('Content is cached for offline use.')
             }
           }
-        };
-      };
+        }
+      }
     })
     .catch(error => {
       console.error('Error during service worker registration:', error)
-    });
+    })
 }
 
 function checkValidServiceWorker(swUrl) {
@@ -85,8 +85,8 @@ function checkValidServiceWorker(swUrl) {
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
             window.location.reload()
-          });
-        });
+          })
+        })
       } else {
         // Service worker found. Proceed as normal.
         registerValidSW(swUrl)
@@ -95,14 +95,14 @@ function checkValidServiceWorker(swUrl) {
     .catch(() => {
       console.log(
         'No internet connection found. App is running in offline mode.'
-      );
-    });
+      )
+    })
 }
 
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
       registration.unregister()
-    });
+    })
   }
 }
